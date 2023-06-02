fdb1103f45e04fb7876d73e1be93d3f6c950841f
surabhi-ca <92444876+surabhi-ca@users.noreply.github.com>
2023-06-02 13:06:18 +0530

    backend

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//D:\Surabhi\workspace\express-proj
 const express=require ('express');
 var bodyParser = require('body-parser');
 const app= express();
@@ -7,7 +6,6 @@ var jsonParser = bodyParser.json();
 var MongoClient=require('mongodb').MongoClient;
 var url="mongodb://localhost:27017/";
 
-//var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -15,30 +13,15 @@ app.use(function(req, res, next) {
     next();
   });
 
-// app.use(express.json);
-
-// app.get('/api',(req,res)=>
-// {
-
-//     res.json({"menu": ["a","b","c"]});
-// });
 
 var obj;
 
 const { v4: uuidv4 } = require('uuid');
 
-// app.get('/api/order',(req,res)=>
-// {
-//   res.send(uuidv4());
-// });
-
 app.post('/api/order',jsonParser,(req,res)=>{
-    //console.log(req.body);
-    //res.send("apple");
-       obj=req.body;
-       //res.send(uuidv4());
-  
-   add_data(obj);
+
+  obj=req.body;
+  add_data(obj);
     
 });
 
@@ -48,7 +31,6 @@ function add_data (item)
   if(err) throw err;
   var dbo=db.db("cake_db");
 
-  //  var myobj={name:"abc",age:"20"};
   dbo.collection("order_summary").insertOne(item,function(err,res){
       if(err) throw err;
       console.log("mongo document created");
@@ -56,11 +38,5 @@ function add_data (item)
   });
 })}
 
-// app.get('/api/order/:order_id',(req,res)=>
-// {
-//     res.send(req.params.order_id);
-// });
-
-// const port=process.env.PORT ;
 const port=5000;
 app.listen(port,()=>console.log(`hi port ${port}`));
