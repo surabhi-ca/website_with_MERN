3b11e3ff589df130cb4bd3d332bc9a697292e196
surabhi-ca <92444876+surabhi-ca@users.noreply.github.com>
2023-06-02 14:11:50 +0530

    Add files via upload

--- frontend/src/App_comp.js
+++ frontend/src/App_comp.js
@@ -5,18 +5,13 @@ import ig from './ig_logo.jpg';
 import ph from './call_logo.png';
 import mail from './mail_logo.jpg';
 import {BrowserRouter as Router,Routes,Route,Redirect,Navigate} from "react-router-dom";
-
-
 import Cont from './components/c_cont';
 import Home from './components/c_home';
-
 import Title from './components/c_title';
 import Ord from './components/c_order';
-
 import Navbut from "./components/navbut";
+
 function Appc() {
-
-
 
 return (
 	<Router>
